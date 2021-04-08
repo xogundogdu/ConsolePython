2445d1257c389ba9df4a99fce8e97b3cef1e2815
xpisiner <63517654+xpisiner@users.noreply.github.com>
2021-04-08 13:26:26 +0300

    Add files via upload
    Request body for update has been created 
    test.csv updated
    Structure deployed for dataframe

--- Console.py
+++ Console.py
@@ -2,6 +2,9 @@ from types import BuiltinMethodType
 import requests
 import json
 import csv
+import pandas as pd
+import numpy as np
+
 EmailsFromCSV=[]
 EmailsFromRequest=[]
 UserIsExist=False
@@ -21,73 +24,91 @@ for person in CSVdata["Resources"]: #get and print email property from json
   CSVEmailValue=CSVEmail[0]
   EmailsFromRequest.append(CSVEmailValue["value"])
       
-with open('test.csv', 'r') as Data:
-    SR = csv.DictReader(Data, delimiter=',')
-    print(SR.line_num)
-    print(SR.fieldnames)
-    print(SR.dialect)
-    print(SR.reader)
-    print(SR.restkey)
-    print(SR.restval)
-    for row in SR:
-        print (row['mail'])
+# with open('test.csv', 'r') as Data:
+#     SR = csv.DictReader(Data, delimiter=',')
+# 	df=pd.DataFrame(data=SR.data.)
+# 	print(df)
+   
+data=pd.read_csv("test.csv")
+print(data.head())
 
 
-print(*EmailsFromCSV,sep = "\n")
+#print(*EmailsFromCSV,sep = "\n")
 print("----------------------------------------------------------------------------")
-print(*EmailsFromRequest,sep = "\n")
+#print(*EmailsFromRequest,sep = "\n")
 
-for mail in EmailsFromCSV:
-  if(mail in EmailsFromRequest):
-    print(mail+" Email eşleşti")
-    print(mail+"'e sahip kullanıcıyı güncelle")
-  else:
-    print(mail+" böyle bir kullanıcı yok")
-    print("csvden kullanıcı oluştur request")
-
-body ={
-	"userName": "123123",
-	"name": {
-		"givenName": "Gokhan",
-		"familyName": "Yilmazturk",
-		"middleName": "Ahmet",
-		"honorificPrefix": "Mr."
-	},
-	"emails": [
-		{
-			"value": "gokhan.yilmazturk@nttdata.com"
-		}
-	],
-	"locale": "TR",
-	"displayName": "Gokhan Yilmazturk NTT",
-	"companyRelationship": "Partner",
-	"company": "SFSF",
-	"department": "Administration",
-	"userType": "partner",
-	"active": "true",
-	"sendMail": "true",
-	"mailVerified": "false",
-	"urn:ietf:params:scim:schemas:extension:enterprise:2.0:User": {
-		"employeeNumber": "JohnS",
-		"costCenter": "costCenter",
-		"organization": "SFSF",
-		"division": "Finance",
-		"department": "Administration",
-		"manager": {
-			"value": "P000002",
-			"$ref": "https://aik7cgkgf.accounts.ondemand.com/service/scim/Users/P000002"
-		}
-	},
-	"urn:sap:cloud:scim:schemas:extension:custom:2.0:User": {
-		"attributes": [
-			{
-				"name": "customAttribute1",
-				"value": "Home Address2"
+for i in range(len(data.axes[0])):   #will be updated users
+	for j  in range(len(data.axes[1])):
+		if(data.iloc[i][j] in EmailsFromRequest):
+			print("mail eşleşti")
+			BodyForUpdate ={
+			"userName": data.iloc[i]["loginName"],
+			"name": {
+				"givenName": data.iloc[i]["firstName"],
+				"familyName": data.iloc[i]["familyName"],
+				"middleName": data.iloc[i]["middleName"],
+				"honorificPrefix": data.iloc[i]["honorificPrefix"]
 			},
-			{
-				"name": "customAttribute2",
-				"value": "Telephone2"
+			"emails": [
+				{
+					"value": data.iloc[i]["mail"]
+				}
+			],
+			"locale": data.iloc[i]["locale"],
+			"displayName": data.iloc[i]["displayName"],
+			"companyRelationship": data.iloc[i]["companyRelationship"],
+			"company": data.iloc[i]["company"],
+			"department": data.iloc[i]["department"],
+			"userType": data.iloc[i]["userType"],
+			"active": data.iloc[i]["status"],
+			"sendMail": data.iloc[i]["sendMail"],
+			"mailVerified": data.iloc[i]["mailVerified"],
+			"urn:ietf:params:scim:schemas:extension:enterprise:2.0:User": {
+				"employeeNumber": data.iloc[i]["employeeNumber"],
+				"costCenter": data.iloc[i]["costCenter"],
+				"organization": data.iloc[i]["organization"],
+				"division": data.iloc[i]["division"],
+				"department": data.iloc[i]["department"],
+				"manager": {
+					"value": data.iloc[i]["managerValue"],
+					"$ref": data.iloc[i]["managerRef"]
+				}
+			},
+			"urn:sap:cloud:scim:schemas:extension:custom:2.0:User": {
+				"attributes": [
+					{
+						"name": "customAttribute01",
+						"value": data.iloc[i]["spCustomAttribute01"]
+					},
+					{
+						"name": "customAttribute02",
+						"value": data.iloc[i]["spCustomAttribute02"]
+					}
+				]
 			}
-		]
-	}
-}
+			}		
+
+			
+			requestURLForUpdate = "https://aik7cgkgf.accounts.ondemand.com/service/scim/Users" 
+			payloadForUpdate={}
+			headersForUpdate = {
+  			'Content-Type': 'application/scim+json',
+  			'Authorization': 'Basic YTE3MzkwOTItODcxMC00NjVhLWI1MjktMWIxMjA2MzRmMWM5OllpbGRpejIwMjA='
+			}
+			responseForUpdate = requests.request("POST", requestURLForUpdate, headers=headersForUpdate, data=BodyForUpdate)
+
+			
+			print(responseForUpdate.status_code)
+
+			data.drop(data.index[i])
+			
+			print(data)
+
+			
+
+		
+
+
+	
+			
+	
