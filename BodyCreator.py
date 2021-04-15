5d85fc6dc90570587349cfe54e8e658ecd932436
xpisiner <63517654+xpisiner@users.noreply.github.com>
2021-04-15 12:26:06 +0300

    working with dynamic body inputs

--- BodyCreator.py
+++ BodyCreator.py
@@ -14,7 +14,7 @@ def BodyCreatorUpdate(data, i, url, token):
 		"Authorization": "Basic "+token,
 		}
 
-	sJson = {
+	bodyForUpdate = {
 		"id": str(data.iloc[i]["id"]),
 		"userName": str(data.iloc[i]["userName"]),
 		"name": {				
@@ -30,9 +30,33 @@ def BodyCreatorUpdate(data, i, url, token):
 
 		},
 		"urn:sap:cloud:scim:schemas:extension:custom:2.0:User": {
-			"attributes": {
+			"attributes": [
+				{
+					"name" : "customAttribute1",
+					"value": ("" if data.iloc[i]["customAttribute1"] == "NONE" else data.iloc[i]["customAttribute1"])
+				},
+				
+				{
+					"name" : "customAttribute2",
+					"value": ("" if data.iloc[i]["customAttribute2"] == "NONE" else data.iloc[i]["customAttribute2"])
+				},
+				
+				{
+					"name" : "customAttribute3",
+					"value": ("" if data.iloc[i]["customAttribute3"] == "NONE" else data.iloc[i]["customAttribute3"])
+				},
+				
+				{
+					"name" : "customAttribute4",
+					"value": ("" if data.iloc[i]["customAttribute4"] == "NONE" else data.iloc[i]["customAttribute4"])
+				},
+				
+				{
+					"name" : "customAttribute5",
+					"value": ("" if data.iloc[i]["customAttribute5"] == "NONE" else data.iloc[i]["customAttribute5"])
+				}
 
-			}
+			]
 			
 			}
 		}
@@ -41,91 +65,50 @@ def BodyCreatorUpdate(data, i, url, token):
     # adding new values to body to update, according to csv input file columns 
 
 	if "givenName" in data.columns:
-	    	sJson["name"].update({"givenName": ""} if data.iloc[i]["givenName"] == "NONE" else {"givenName": data.iloc[i]["givenName"]})
+	    	bodyForUpdate["name"].update({"givenName": ""} if data.iloc[i]["givenName"] == "NONE" else {"givenName": data.iloc[i]["givenName"]})
 	if "familyName" in data.columns:
-	    	sJson["name"].update({"familyName": ""} if data.iloc[i]["familyName"] == "NONE" else {"familyName": data.iloc[i]["familyName"]})
+	    	bodyForUpdate["name"].update({"familyName": ""} if data.iloc[i]["familyName"] == "NONE" else {"familyName": data.iloc[i]["familyName"]})
 	if "honorificPrefix" in data.columns:
-	        sJson["name"].update({"honorificPrefix": ""} if data.iloc[i]["honorificPrefix"] == "NONE" else {"honorificPrefix": data.iloc[i]["honorificPrefix"]})
+	        bodyForUpdate["name"].update({"honorificPrefix": ""} if data.iloc[i]["honorificPrefix"] == "NONE" else {"honorificPrefix": data.iloc[i]["honorificPrefix"]})
 	if "middleName" in data.columns:
-	    	sJson["name"].update({"middleName": ""} if data.iloc[i]["middleName"] == "NONE" else {"middleName": data.iloc[i]["middleName"]})
+	    	bodyForUpdate["name"].update({"middleName": ""} if data.iloc[i]["middleName"] == "NONE" else {"middleName": data.iloc[i]["middleName"]})
 	if "locale" in data.columns:
-	    	sJson.update({"locale": ""} if data.iloc[i]["locale"] == "NONE" else {"locale": data.iloc[i]["locale"]})
+	    	bodyForUpdate.update({"locale": ""} if data.iloc[i]["locale"] == "NONE" else {"locale": data.iloc[i]["locale"]})
 	if "displayName" in data.columns:
-	    	sJson.update({"displayName": ""} if data.iloc[i]["displayName"] == "NONE" else {"displayName": data.iloc[i]["displayName"]})
+	    	bodyForUpdate.update({"displayName": ""} if data.iloc[i]["displayName"] == "NONE" else {"displayName": data.iloc[i]["displayName"]})
 	if "companyRelationship" in data.columns:
-	    	sJson.update({"companyRelationship": ""} if data.iloc[i]["companyRelationship"] == "NONE" else {"companyRelationship": data.iloc[i]["companyRelationship"]})
+	    	bodyForUpdate.update({"companyRelationship": ""} if data.iloc[i]["companyRelationship"] == "NONE" else {"companyRelationship": data.iloc[i]["companyRelationship"]})
 	if "company" in data.columns:
-	    	sJson.update({"company": ""} if data.iloc[i]["company"] == "NONE" else {"company": data.iloc[i]["company"]})
+	    	bodyForUpdate.update({"company": ""} if data.iloc[i]["company"] == "NONE" else {"company": data.iloc[i]["company"]})
 	if "department" in data.columns:
-	    	sJson.update({"department": ""} if data.iloc[i]["department"] == "NONE" else {"department": data.iloc[i]["department"]})
+	    	bodyForUpdate.update({"department": ""} if data.iloc[i]["department"] == "NONE" else {"department": data.iloc[i]["department"]})
 	if "userType" in data.columns:
-	    	sJson.update({"userType": ""} if data.iloc[i]["userType"] == "NONE" else {"userType": data.iloc[i]["userType"]})			
+	    	bodyForUpdate.update({"userType": ""} if data.iloc[i]["userType"] == "NONE" else {"userType": data.iloc[i]["userType"]})			
 	if "status" in data.columns:
-	    	sJson.update({"status": ""} if data.iloc[i]["status"] == "NONE" else {"status": str(data.iloc[i]["status"])})
+	    	bodyForUpdate.update({"status": ""} if data.iloc[i]["status"] == "NONE" else {"status": str(data.iloc[i]["status"])})
 	if "sendMail" in data.columns:
-	    	sJson.update({"sendMail": ""} if data.iloc[i]["sendMail"] == "NONE" else {"sendMail": str(data.iloc[i]["sendMail"])})
+	    	bodyForUpdate.update({"sendMail": ""} if data.iloc[i]["sendMail"] == "NONE" else {"sendMail": str(data.iloc[i]["sendMail"])})
 	if "mailVerified" in data.columns:
-	    	sJson.update({"mailVerified": ""} if data.iloc[i]["mailVerified"] == "NONE" else {"mailVerified": str(data.iloc[i]["mailVerified"])})
+	    	bodyForUpdate.update({"mailVerified": ""} if data.iloc[i]["mailVerified"] == "NONE" else {"mailVerified": str(data.iloc[i]["mailVerified"])})
 	if "employeeNumber" in data.columns:
-	    	sJson["urn:ietf:params:scim:schemas:extension:enterprise:2.0:User"].update({"employeeNumber": data.iloc[i]["employeeNumber"]})
+	    	bodyForUpdate["urn:ietf:params:scim:schemas:extension:enterprise:2.0:User"].update({"employeeNumber": data.iloc[i]["employeeNumber"]})
 	if "costCenter" in data.columns:
-	    	sJson["urn:ietf:params:scim:schemas:extension:enterprise:2.0:User"].update({"costCenter": data.iloc[i]["costCenter"]})
+	    	bodyForUpdate["urn:ietf:params:scim:schemas:extension:enterprise:2.0:User"].update({"costCenter": data.iloc[i]["costCenter"]})
 	if "organization" in data.columns:
-	    	sJson["urn:ietf:params:scim:schemas:extension:enterprise:2.0:User"].update({"organization": data.iloc[i]["organization"]})
+	    	bodyForUpdate["urn:ietf:params:scim:schemas:extension:enterprise:2.0:User"].update({"organization": data.iloc[i]["organization"]})
 	if "division" in data.columns:
-	    	sJson["urn:ietf:params:scim:schemas:extension:enterprise:2.0:User"].update({"division": data.iloc[i]["division"]})
+	    	bodyForUpdate["urn:ietf:params:scim:schemas:extension:enterprise:2.0:User"].update({"division": data.iloc[i]["division"]})
 	if "department" in data.columns:
-	    	sJson["urn:ietf:params:scim:schemas:extension:enterprise:2.0:User"].update({"department": data.iloc[i]["department"]})
+	    	bodyForUpdate["urn:ietf:params:scim:schemas:extension:enterprise:2.0:User"].update({"department": data.iloc[i]["department"]})
 	if "managerValue" in data.columns:
-			sJson["urn:ietf:params:scim:schemas:extension:enterprise:2.0:User"]["manager"][0].update({"value": data.iloc[i]["managerValue"]})
+			bodyForUpdate["urn:ietf:params:scim:schemas:extension:enterprise:2.0:User"]["manager"].update({"value": data.iloc[i]["managerValue"]})
 	if "managerRef" in data.columns:
-			sJson["urn:ietf:params:scim:schemas:extension:enterprise:2.0:User"]["manager"][0].update({"$ref": str(data.iloc[i]["managerRef"])})
+			bodyForUpdate["urn:ietf:params:scim:schemas:extension:enterprise:2.0:User"]["manager"].update({"$ref": str(data.iloc[i]["managerRef"])})
 	
-	#custom attributes (max 10 fields can be utilized):
-	if "CustomAttribute1name" in data.columns:
-			sJson["urn:sap:cloud:scim:schemas:extension:custom:2.0:User"]["attributes"][0].update({"name": data.iloc[0]["CustomAttribute1name"]})
-	if "CustomAttribute1value" in data.columns:
-			sJson["urn:sap:cloud:scim:schemas:extension:custom:2.0:User"]["attributes"][0].update({"value": data.iloc[0]["CustomAttribute1value"]})
-	if "CustomAttribute2name" in data.columns:
-			sJson["urn:sap:cloud:scim:schemas:extension:custom:2.0:User"]["attributes"][1].update({"name": data.iloc[1]["CustomAttribute2name"]})
-	if "CustomAttribute2value" in data.columns:
-			sJson["urn:sap:cloud:scim:schemas:extension:custom:2.0:User"]["attributes"][1].update({"value": data.iloc[1]["CustomAttribute2value"]})
-	if "CustomAttribute3name" in data.columns:
-			sJson["urn:sap:cloud:scim:schemas:extension:custom:2.0:User"]["attributes"][2].update({"name": data.iloc[2]["CustomAttribute3name"]})
-	if "CustomAttribute3value" in data.columns:
-			sJson["urn:sap:cloud:scim:schemas:extension:custom:2.0:User"]["attributes"][2].update({"value": data.iloc[2]["CustomAttribute3value"]})
-	if "CustomAttribute4name" in data.columns:
-			sJson["urn:sap:cloud:scim:schemas:extension:custom:2.0:User"]["attributes"][3].update({"name": data.iloc[3]["CustomAttribute4name"]})
-	if "CustomAttribute4value" in data.columns:
-			sJson["urn:sap:cloud:scim:schemas:extension:custom:2.0:User"]["attributes"][3].update({"value": data.iloc[3]["CustomAttribute4value"]})
-	if "CustomAttribute5name" in data.columns:
-			sJson["urn:sap:cloud:scim:schemas:extension:custom:2.0:User"]["attributes"][4].update({"name": data.iloc[4]["CustomAttribute5name"]})
-	if "CustomAttribute5value" in data.columns:
-			sJson["urn:sap:cloud:scim:schemas:extension:custom:2.0:User"]["attributes"][4].update({"value": data.iloc[4]["CustomAttribute5value"]})
-	if "CustomAttribute6name" in data.columns:
-			sJson["urn:sap:cloud:scim:schemas:extension:custom:2.0:User"]["attributes"][5].update({"name": data.iloc[5]["CustomAttribute6name"]})
-	if "CustomAttribute6value" in data.columns:
-			sJson["urn:sap:cloud:scim:schemas:extension:custom:2.0:User"]["attributes"][5].update({"value": data.iloc[5]["CustomAttribute6value"]})
-	if "CustomAttribute7name" in data.columns:
-			sJson["urn:sap:cloud:scim:schemas:extension:custom:2.0:User"]["attributes"][6].update({"name": data.iloc[6]["CustomAttribute7name"]})
-	if "CustomAttribute7value" in data.columns:
-			sJson["urn:sap:cloud:scim:schemas:extension:custom:2.0:User"]["attributes"][6].update({"value": data.iloc[6]["CustomAttribute7value"]})
-	if "CustomAttribute8name" in data.columns:
-			sJson["urn:sap:cloud:scim:schemas:extension:custom:2.0:User"]["attributes"][7].update({"name": data.iloc[7]["CustomAttribute8name"]})
-	if "CustomAttribute8value" in data.columns:
-			sJson["urn:sap:cloud:scim:schemas:extension:custom:2.0:User"]["attributes"][7].update({"value": data.iloc[7]["CustomAttribute8value"]})
-	if "CustomAttribute9name" in data.columns:
-			sJson["urn:sap:cloud:scim:schemas:extension:custom:2.0:User"]["attributes"][8].update({"name": data.iloc[8]["CustomAttribute9name"]})
-	if "CustomAttribute9value" in data.columns:
-			sJson["urn:sap:cloud:scim:schemas:extension:custom:2.0:User"]["attributes"][8].update({"value": data.iloc[8]["CustomAttribute9value"]})
-	if "CustomAttribute10name" in data.columns:
-			sJson["urn:sap:cloud:scim:schemas:extension:custom:2.0:User"]["attributes"][9].update({"name": data.iloc[9]["CustomAttribute10name"]})
-	if "CustomAttribute10value" in data.columns:
-			sJson["urn:sap:cloud:scim:schemas:extension:custom:2.0:User"]["attributes"][9].update({"value": data.iloc[9]["CustomAttribute10value"]})
-
+	
 
 	# parse body object to json
-	BodyForUpdate = json.dumps(sJson)
+	BodyForUpdate = json.dumps(bodyForUpdate)
 
 	print(BodyForUpdate)
 	
@@ -138,55 +121,107 @@ def BodyCreatorUpdate(data, i, url, token):
 
 
 # create json body for creating new user
+# initial body only includes required values
 def BodyCreatorCreate(data, i, url, token):
+
+	# set authorization settings
 	headersForCreate = {
 		"Content-Type": "application/scim+json",
 		"Authorization": "Basic "+token,
 	}
-	BodyForCreate = json.dumps({
+
+	bodyForCreate = {
 		"userName": str(data.iloc[i]["userName"]),
-		"name": {
-			"givenName": data.iloc[i]["givenName"],
-			"familyName": data.iloc[i]["familyName"],
-			"middleName": data.iloc[i]["middleName"],
-			"honorificPrefix": data.iloc[i]["honorificPrefix"]
+		"name": {				
 		},
 		"emails": [
-			{"value": data.iloc[i]["mailValue"]}
-		],
-		"locale": data.iloc[i]["locale"],
-		"displayName": data.iloc[i]["displayName"],
-		"companyRelationship": data.iloc[i]["companyRelationship"],
-		"company": data.iloc[i]["company"],
-		"department": data.iloc[i]["department"],
-		"userType": data.iloc[i]["userType"],
-		"active": True,
-		"sendMail": str(data.iloc[i]["sendMail"]),
-		"mailVerified": str(data.iloc[i]["mailVerified"]),
-		"urn:ietf:params:scim:schemas:extension:enterprise:2.0:User": {
-			"employeeNumber": data.iloc[i]["employeeNumber"],
-			"costCenter": data.iloc[i]["costCenter"],
-			"organization": data.iloc[i]["organization"],
-			"division": data.iloc[i]["division"],
-			"department": data.iloc[i]["department"],
-			"manager": {
-				"value": data.iloc[i]["managerValue"],
-				"$ref": str(data.iloc[i]["managerRef"])
+			{
+				"value": data.iloc[i]["mailValue"]
 			}
+		],
+		"urn:ietf:params:scim:schemas:extension:enterprise:2.0:User": {	
+			"manager": {
+			}	
+
 		},
 		"urn:sap:cloud:scim:schemas:extension:custom:2.0:User": {
 			"attributes": [
 				{
-					"name": ("" if data.iloc[0]["CustomAttribute1name"] == "NONE" else data.iloc[0]["CustomAttribute1name"]),
-					"value":("" if data.iloc[0]["CustomAttribute1value"] == "NONE" else data.iloc[0]["CustomAttribute1value"])
+					"name" : "customAttribute1",
+					"value": ("" if data.iloc[i]["customAttribute1"] == "NONE" else data.iloc[i]["customAttribute1"])
 				},
+				
 				{
-					"name": ("" if data.iloc[0]["CustomAttribute2name"] == "NONE" else data.iloc[0]["CustomAttribute2name"]),
-					"value":("" if data.iloc[0]["CustomAttribute2value"] == "NONE" else data.iloc[0]["CustomAttribute2value"])
+					"name" : "customAttribute2",
+					"value": ("" if data.iloc[i]["customAttribute2"] == "NONE" else data.iloc[i]["customAttribute2"])
+				},
+				
+				{
+					"name" : "customAttribute3",
+					"value": ("" if data.iloc[i]["customAttribute3"] == "NONE" else data.iloc[i]["customAttribute3"])
+				},
+				
+				{
+					"name" : "customAttribute4",
+					"value": ("" if data.iloc[i]["customAttribute4"] == "NONE" else data.iloc[i]["customAttribute4"])
+				},
+				
+				{
+					"name" : "customAttribute5",
+					"value": ("" if data.iloc[i]["customAttribute5"] == "NONE" else data.iloc[i]["customAttribute5"])
 				}
+
 			]
+			
+			}
 		}
-		})
+
+	# adding new values to body to update, according to csv input file columns
+	if "givenName" in data.columns:
+	    	bodyForCreate["name"].update({"givenName": ""} if data.iloc[i]["givenName"] == "NONE" else {"givenName": data.iloc[i]["givenName"]})
+	if "familyName" in data.columns:
+	    	bodyForCreate["name"].update({"familyName": ""} if data.iloc[i]["familyName"] == "NONE" else {"familyName": data.iloc[i]["familyName"]})
+	if "honorificPrefix" in data.columns:
+	        bodyForCreate["name"].update({"honorificPrefix": ""} if data.iloc[i]["honorificPrefix"] == "NONE" else {"honorificPrefix": data.iloc[i]["honorificPrefix"]})
+	if "middleName" in data.columns:
+	    	bodyForCreate["name"].update({"middleName": ""} if data.iloc[i]["middleName"] == "NONE" else {"middleName": data.iloc[i]["middleName"]})
+	if "locale" in data.columns:
+	    	bodyForCreate.update({"locale": ""} if data.iloc[i]["locale"] == "NONE" else {"locale": data.iloc[i]["locale"]})
+	if "displayName" in data.columns:
+	    	bodyForCreate.update({"displayName": ""} if data.iloc[i]["displayName"] == "NONE" else {"displayName": data.iloc[i]["displayName"]})
+	if "companyRelationship" in data.columns:
+	    	bodyForCreate.update({"companyRelationship": ""} if data.iloc[i]["companyRelationship"] == "NONE" else {"companyRelationship": data.iloc[i]["companyRelationship"]})
+	if "company" in data.columns:
+	    	bodyForCreate.update({"company": ""} if data.iloc[i]["company"] == "NONE" else {"company": data.iloc[i]["company"]})
+	if "department" in data.columns:
+	    	bodyForCreate.update({"department": ""} if data.iloc[i]["department"] == "NONE" else {"department": data.iloc[i]["department"]})
+	if "userType" in data.columns:
+	    	bodyForCreate.update({"userType": ""} if data.iloc[i]["userType"] == "NONE" else {"userType": data.iloc[i]["userType"]})			
+	if "status" in data.columns:
+	    	bodyForCreate.update({"status": ""} if data.iloc[i]["status"] == "NONE" else {"status": str(data.iloc[i]["status"])})
+	if "sendMail" in data.columns:
+	    	bodyForCreate.update({"sendMail": ""} if data.iloc[i]["sendMail"] == "NONE" else {"sendMail": str(data.iloc[i]["sendMail"])})
+	if "mailVerified" in data.columns:
+	    	bodyForCreate.update({"mailVerified": ""} if data.iloc[i]["mailVerified"] == "NONE" else {"mailVerified": str(data.iloc[i]["mailVerified"])})
+	if "employeeNumber" in data.columns:
+	    	bodyForCreate["urn:ietf:params:scim:schemas:extension:enterprise:2.0:User"].update({"employeeNumber": data.iloc[i]["employeeNumber"]})
+	if "costCenter" in data.columns:
+	    	bodyForCreate["urn:ietf:params:scim:schemas:extension:enterprise:2.0:User"].update({"costCenter": data.iloc[i]["costCenter"]})
+	if "organization" in data.columns:
+	    	bodyForCreate["urn:ietf:params:scim:schemas:extension:enterprise:2.0:User"].update({"organization": data.iloc[i]["organization"]})
+	if "division" in data.columns:
+	    	bodyForCreate["urn:ietf:params:scim:schemas:extension:enterprise:2.0:User"].update({"division": data.iloc[i]["division"]})
+	if "department" in data.columns:
+	    	bodyForCreate["urn:ietf:params:scim:schemas:extension:enterprise:2.0:User"].update({"department": data.iloc[i]["department"]})
+	if "managerValue" in data.columns:
+			bodyForCreate["urn:ietf:params:scim:schemas:extension:enterprise:2.0:User"]["manager"].update({"value": data.iloc[i]["managerValue"]})
+	if "managerRef" in data.columns:
+			bodyForCreate["urn:ietf:params:scim:schemas:extension:enterprise:2.0:User"]["manager"].update({"$ref": str(data.iloc[i]["managerRef"])})
+	
+
+	# parse body object to json
+	BodyForCreate = json.dumps(bodyForCreate)
+	
 	# send request for creating user
 	response = requests.request("POST", url, headers=headersForCreate, data=BodyForCreate)
 
--- Console.py
+++ Console.py
@@ -36,7 +36,7 @@ for person in CSVdata["Resources"]:
   EmailsFromRequest.append(CSVEmailValue["value"])
        
 # read input csv data and replace nan fields with "NONE" for later operations
-data = pd.read_csv("example.csv").fillna("NONE")
+data = pd.read_csv("import.csv").fillna("NONE")
 
 # creating json bodies: 
 # if mail from input csv file matches with any mail from get users request list, then update. If no match, then create new user. 
@@ -44,6 +44,6 @@ for i in range(len(data.axes[0])):
 		if(data.iloc[i]["mailValue"] in EmailsFromRequest): #update user
 			url = "https://"+tenantID+".accounts.ondemand.com/service/scim/Users/"+data.iloc[i]["id"]
 			BodyCreator.BodyCreatorUpdate(data,i,url,token)
-		else:#create user 
+		else: #create user 
 			url = "https://"+tenantID+".accounts.ondemand.com/service/scim/Users"			
 			BodyCreator.BodyCreatorCreate(data,i,url,token)
